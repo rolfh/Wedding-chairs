e7b0be7cc6368fb8313588659efdf5bf13cb7c4b
Rolf Hammerstad <rolfh@users.noreply.github.com>
2019-09-04 20:17:55 +0200

    Name corrections, svg table elongated

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 var vm = new Vue({
-	el: '#app',
-	data() {
-		return {
-			chairs: [],
-			bait: false,
-			hide: false,
-			positions: false
-		}
-	},
-	mounted() {
-		fetch('navn.json')
-			.then(blob => blob.json())
-			.then(data => {
-				this.chairs = data
-			})
-		document.addEventListener('keydown', event => {
-			if (event.key == 'h') {
-				this.hide = !this.hide
-			}
-			if (event.key == 'b') {
-				this.bait = !this.bait
-			}
-			if (event.key == 'p') {
-				this.positions = !this.positions
-			}
-		})
-	}
+  el: '#app',
+  data() {
+    return {
+      chairs: [],
+      bait: false,
+      hide: false,
+      positions: false
+    }
+  },
+  mounted() {
+    fetch('navn.json')
+      .then(blob => blob.json())
+      .then(data => {
+        this.chairs = data
+      })
+    document.addEventListener('keydown', event => {
+      // if (event.key == 'h') {
+      // 	this.hide = !this.hide
+      // }
+      if (event.key == 'b') {
+        this.bait = !this.bait
+      }
+      if (event.key == 'p') {
+        this.positions = !this.positions
+      }
+    })
+  }
 })
